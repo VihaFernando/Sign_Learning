d615a28fcc611c099330fc0db2f1bde18b347a0b
VihaFernando <vihanga.20220051@gmail.com>
2024-02-07 14:27:06 +0530

    modified

---
+++ src/components/something.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './something.css'
+
+function Something({message}) {
+  return (
+    <div className='wrapper'>
+      <h1>{message}</h1>
+    </div>
+  )
+}
+
+export default Something
+
