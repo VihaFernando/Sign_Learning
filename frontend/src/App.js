ce84473510cca395b3ed34ff6a0028d81bc88bf2
KaviEkanayake <kavindi.20221327@iit.ac.lk>
2024-03-08 14:17:45 +0530

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ContactUs from './components/ContactUs';
 import Level1 from './components/Level1';
 import Level2 from './components/Level2';
 import Level3 from './components/Level3';
+import Level4 from './components/Level4';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path="/Level1" element={<Level1 />} />
           <Route path="/Level2" element={<Level2/>} />
           <Route path="/Level3" element={<Level3/>} />
+          <Route path="/Level4" element={<Level4/>}/>
         </Routes>
       </div>
     </Router>
